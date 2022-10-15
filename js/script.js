0aa198f7e83050130eeb68b65be04a97245aee99
Igor <example@gmail.com>
2022-10-15 19:23:58 +0300

    application

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-let number = 5;
-const leftBorderWidth = 1;
-
-number = 10;
-let name = "Igor"
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+const lastFilmScore = prompt('На сколько оцените его?', '');
+const lastFilm2 = prompt('Один из последних просмотренных фильмов?', '');
+const lastFilmScore2 = prompt('На сколько оцените его?', '');
+personalMovieDB.movies[lastFilm] = lastFilmScore;
+personalMovieDB.movies[lastFilm2] = lastFilmScore2;
+console.log(personalMovieDB);
